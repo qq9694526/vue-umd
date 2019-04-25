67d92de6faa829cbecd416f4dbc1a143bebaa130
qq9694526 <9694526@qq.com>
2019-04-25 09:34:40 +0800

    first commit

---
+++ components/nav.js
@@ -0,0 +1,9 @@
+
+
+// 定义一个名为 button-counter 的新组件
+Vue.component('nav-header', {
+    template: `<p>
+        <router-link to="/foo">Go to Foo</router-link>
+        <router-link to="/bar">Go to Bar</router-link>
+    </p>`
+})
